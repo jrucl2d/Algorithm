4e77876031465fa62492668766bf2c356136e3ba
jrucl2d <ysk789465@gmail.com>
2021-08-17 21:46:54 +0900

    풀이 성공. DP를 사용

---
+++ 분류별/Dynamic Programming/구간_합_구하기5_11660.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Solution solution = new Solution();
+        solution.solution();
+    }
+    static class Solution {
+        int n, m;
+        int[][] arr, dp;
+
+        public void solution() throws IOException {
+            input();
+        }
+
+        void input() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new int[n][n];
+            dp = new int[n + 1][n + 1];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] += Integer.parseInt(st.nextToken());
+                    dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1] - dp[i][j] + arr[i][j];
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x1 = Integer.parseInt(st.nextToken());
+                int y1 = Integer.parseInt(st.nextToken());
+                int x2 = Integer.parseInt(st.nextToken());
+                int y2 = Integer.parseInt(st.nextToken());
+                System.out.println(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]);
+            }
+        }
+    }
+}
+
